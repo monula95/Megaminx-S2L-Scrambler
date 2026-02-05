6a7991dd0fb890174324b000294ada98b90b860b
Andre <espietandre@gmail.com>
2026-02-04 19:26:14 -0600

    fix html formatting

--- app.py
+++ app.py
@@ -9,5 +9,7 @@ length = st.number_input("Scramble length", min_value=1, value=49)
 if st.button("Generate Scramble"):
     scramble_str = mega.scramble(length)
     
-    # Display as preformatted code in Markdown
-    st.markdown(f"<pre style='font-family: monospace; font-size:16px'>{scramble_str}</pre>", unsafe_allow_html=True)
+    # Replace newlines with <br> so HTML renders them
+    scramble_html = scramble_str.replace("\n", "<br>")
+    
+    st.markdown(f"<pre style='font-family: monospace; font-size:16px'>{scramble_html}</pre>", unsafe_allow_html=True)
