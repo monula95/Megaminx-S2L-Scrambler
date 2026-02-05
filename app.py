701bea9ba53f49eef2763fe6d8425b8956b6c3a5
Andre <espietandre@gmail.com>
2026-02-04 19:28:58 -0600

    change font

--- app.py
+++ app.py
@@ -9,7 +9,7 @@ length = st.number_input("Scramble length", min_value=1, value=49)
 if st.button("Generate Scramble"):
     scramble_str = mega.scramble(length)
     
-    # Replace newlines with <br> so HTML renders them
-    scramble_html = scramble_str.replace("\n", "<br>")
-    
-    st.markdown(f"<pre style='font-family: monospace; font-size:25px'>{scramble_html}</pre>", unsafe_allow_html=True)
+    st.markdown(
+        f"<div style='font-family:monospace; font-size:20px; white-space:pre'>{scramble_str}</div>", 
+        unsafe_allow_html=True
+    )
